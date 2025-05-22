21b85394a3544b3c2c1a1e743b08932e2e5679a1
minel <minel@planetek.it>
2025-05-22 17:31:17 +0200

    test

--- {{cookiecutter.workflow_id}}/service.py
+++ {{cookiecutter.workflow_id}}/service.py
@@ -112,6 +112,8 @@ class SimpleExecutionHandler(ExecutionHandler):
         stageout_absolute_path = os.path.abspath(stageout_file.name)
         logger.info(f"stageout_file: {stageout_file.name}")
         logger.info(f"stageout_absolute_path: {stageout_absolute_path}")
+        for line in stageout_file:
+            logger.info(line, end='')
         stageout_yaml = yaml.safe_load(stageout_file)
 
         # Depending on the thematic service name, you can update the stageout.yaml file.
