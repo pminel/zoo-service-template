0bb60ed7b1bed716f8a0d071d5d9c136fb7fb8c1
minel <minel@planetek.it>
2025-05-22 17:29:09 +0200

    test

--- {{cookiecutter.workflow_id}}/service.py
+++ {{cookiecutter.workflow_id}}/service.py
@@ -110,6 +110,8 @@ class SimpleExecutionHandler(ExecutionHandler):
         # you can first load the stageout.yaml file from the assets directory.
         stageout_file = open("/assets/stageout.yaml","rb")
         stageout_absolute_path = os.path.abspath(stageout_file.name)
+        logger.info(f"stageout_file: {stageout_file.name}")
+        logger.info(f"stageout_absolute_path: {stageout_absolute_path}")
         stageout_yaml = yaml.safe_load(stageout_file)
 
         # Depending on the thematic service name, you can update the stageout.yaml file.
