108081558a6bf2025de9bb195ad5b44efb3d468d
minel <minel@planetek.it>
2025-05-23 07:06:13 +0200

    test

---
+++ assets/custom_stageout.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+print("GOOD MORNING")
+print(f"args: {sys.argv}")
--- {{cookiecutter.workflow_id}}/service.py
+++ {{cookiecutter.workflow_id}}/service.py
@@ -102,9 +102,9 @@ class SimpleExecutionHandler(ExecutionHandler):
         logger.info(f"thematic_service_name: {thematic_service_name}")
         self.conf["additional_parameters"]["thematic_service_name"] = thematic_service_name
 
-        # s3_bucket = self.get_s3_bucket()
-        # logger.info(f"s3_bucket: {s3_bucket}")
-        # self.conf["additional_parameters"]["s3_bucket"] = s3_bucket
+        s3_bucket = self.get_s3_bucket()
+        logger.info(f"s3_bucket: {s3_bucket}")
+        self.conf["additional_parameters"]["s3_bucket"] = s3_bucket
 
         # In this example, you want to create a stageout.yaml file based on the service name,
         # you can first load the stageout.yaml file from the assets directory.
@@ -113,27 +113,27 @@ class SimpleExecutionHandler(ExecutionHandler):
         logger.info(f"stageout_file: {stageout_file.name}")
         logger.info(f"stageout_absolute_path: {stageout_absolute_path}")
         stageout_yaml = yaml.safe_load(stageout_file)
-        logger.info(f"stageout_yaml: {stageout_yaml}")
 
-        # Depending on the thematic service name, you can update the stageout.yaml file.
-        # For example, if you want to import a specific Python file based on the thematic service name,
-        # you can do it like this (obviously, you can also add new code to the stageout):
-        entries = stageout_yaml["requirements"]["InitialWorkDirRequirement"]['listing']
-        entries[0]["entry"] += "\n" +\
-            "try:\n" +\
-            "    import my_service_indexing\n" +\
-            "except ImportError as e:\n" +\
-            "    print('error loading dynamic content: '+str(e),file=sys.stderr)\n"
-        #
-        # In the stageout.yaml file, we have put a dedicated [INIT_TEMPLATE] string for you to replace with your code.
-        # This is useful if you want to add new code to the intial phase of the stage.py file (which is the entry 0).
-        # In the example below, we are adding a new variable named thematic_service_name.
-        #
-        entries[0]["entry"] = entries[0]["entry"].replace(
-            "[INIT_TEMPLATE]",
-            "thematic_service_name=sys.argv[4]\n" +
-            "print(f\"thematic_service_name: {thematic_service_name}\", file=sys.stderr)"
-        )
+        # # Depending on the thematic service name, you can update the stageout.yaml file.
+        # # For example, if you want to import a specific Python file based on the thematic service name,
+        # # you can do it like this (obviously, you can also add new code to the stageout):
+        # entries = stageout_yaml["requirements"]["InitialWorkDirRequirement"]['listing']
+        # entries[0]["entry"] += "\n" +\
+        #     "try:\n" +\
+        #     "    import my_service_indexing\n" +\
+        #     "except ImportError as e:\n" +\
+        #     "    print('error loading dynamic content: '+str(e),file=sys.stderr)\n"
+        # #
+        # # In the stageout.yaml file, we have put a dedicated [INIT_TEMPLATE] string for you to replace with your code.
+        # # This is useful if you want to add new code to the intial phase of the stage.py file (which is the entry 0).
+        # # In the example below, we are adding a new variable named thematic_service_name.
+        # #
+        # entries[0]["entry"] = entries[0]["entry"].replace(
+        #     "[INIT_TEMPLATE]",
+        #     "thematic_service_name=sys.argv[4]\n" +
+        #     "print(f\"thematic_service_name: {thematic_service_name}\", file=sys.stderr)"
+        # )
+
         # We are not doing it here, but you can also add new code to the stageout.yaml file.
         #
         # You can also add new Python file (or anythign else) to the stageout.yaml file.
@@ -153,17 +153,26 @@ class SimpleExecutionHandler(ExecutionHandler):
         #
         # In this example, you should add the new my_service_indexing.py file imported at the end of the .
         #
+        # entries.append(
+        #     {
+        #         "entryname": "my_service_indexing.py",
+        #         "entry": "import sys\n"+
+        #             f"print('Hello from {thematic_service_name}',file=sys.stderr)\n"+
+        #             "print(sys.argv,file=sys.stderr)"
+        #     }
+        # )
         entries.append(
             {
-                "entryname": "my_service_indexing.py",
-                "entry": "import sys\n"+
-                    f"print('Hello from {thematic_service_name}',file=sys.stderr)\n"+
-                    "print(sys.argv,file=sys.stderr)"
+                "entryname": "custom_stageout.py",
+                "entry": open("/assets/custom_stageout.py","rb").read().decode("utf-8")
             }
         )
         # You can also add new input parameters and passs it as an argument.
         stageout_yaml["inputs"]["thematic_service_name"]={"type": "string"}
         stageout_yaml["arguments"].append("$( inputs.thematic_service_name )")
+        stageout_yaml["inputs"]["s3_bucket"]={"type": "string"}
+        stageout_yaml["arguments"].append("$( inputs.s3_bucket )")
+        
         # Yet another input parameter to be passed to the wrapped Application Package.
         self.conf["additional_parameters"]["my_new_parameter"] = "my-service-name"
         #
@@ -182,9 +191,8 @@ class SimpleExecutionHandler(ExecutionHandler):
         #
         self.stageout_file_path = f"/{self.conf['main']['tmpPath']}/stageout{self.conf['lenv']['usid']}.yaml"
         logger.info(f"self.stageout_file_path: {self.stageout_file_path}")
-        
+
         stageout_file=open(self.stageout_file_path,"w")
-        logger.info(f"stageout_yaml 2: {stageout_yaml}")
         yaml.dump(stageout_yaml,stageout_file)
         stageout_file.close()
         #
